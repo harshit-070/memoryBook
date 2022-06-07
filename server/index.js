e5e3eb2ecfe496b19588cddf22232d1c0127f271
Harshit <harshitmittal070@gmail.com>
2022-06-07 20:24:57 +0530

    Change the dburl

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
@@ -10,11 +11,11 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+dotenv.config();
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://memoryBook:mmbook@cluster0.6ecoz.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.DB_STRING;
 const PORT = process.env.PORT || 5000;
 
 mongoose
